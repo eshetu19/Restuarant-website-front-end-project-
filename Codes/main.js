e106a7232f037ee470739b314a3df5de341c6e15
eshetu19 <eshetsegaye87@gmail.com>
2026-02-24 08:44:21 -0800

    Remove unused code for menu and loader features
    Removed commented-out code for scroll feature and loader.

--- Codes/main.js
+++ Codes/main.js
@@ -4,22 +4,3 @@ menu.onclick = () => {
   menu.classList.toggle("fa-times");
   navbar.classList.toggle("active");
 };
-/*for x feature on menu bar
-window.onscroll=() =>{
-  menu.classList.remove('fa-times');
-  navbar.classList.remove('active');
-  if(window.scrollY > 60){
-    document.querySelector('#scroll-top').classList.add('active');
-  }else{
-    document.querySelector('#scroll-top').classList.remove('active');
-  }
-}
- 
- function loader(){
-  document.querySelector('.loader-container').classList.add('fade-out');
- }
- function fadeout(){
-  setInterval(loader,3000);
- }
-window.onload = fadeout();
- */
